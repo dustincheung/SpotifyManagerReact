42aa75cf140c192b00cf46020bcfe20c9841b64d
Dustin Cheung <dustincheung@Dustins-MacBook-Pro.local>
2020-05-17 00:51:59 -0400

    uncommented proxy, and appended REACT_APP to env vars

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ import history from "../history";
 export const getCurrUser = () => {
 	return async (dispatch) => {
 		console.log("GET REQUEST:" + process.env.REACT_APP_BACKEND_URI + "/api/current_user");
-		const response = await axios.get(process.env.REACT_APP_BACKEND_URI + "/api/current_user");
+		const response = await axios.get("/api/current_user");
 		console.log("action creator payload:")
 		console.log(response.data[0]);
 
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// module.exports = function (app) {
-//   app.use(
-//     ["/api/*", "/login"],			//requests to these routes will automatically redirect to express 5000 server
-//     createProxyMiddleware({
-//       target: process.env.BACKEND_URI || 'http://localhost:5000',
-//     })
-//   );
-// };
+module.exports = function (app) {
+  app.use(
+     ["/api/*", "/login"],			//requests to these routes will automatically redirect to express 5000 server
+     createProxyMiddleware({
+       target: process.env.REACT_APP_BACKEND_URI || 'http://localhost:5000',
+     })
+   );
+};
