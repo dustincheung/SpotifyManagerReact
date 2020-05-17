e65c6eb8ef49c99956c9ca1c594075cee6152309
Dustin Cheung <dustincheung@Dustins-MacBook-Pro.local>
2020-05-17 00:21:46 -0400

    commented out proxy, and appened REACT_APP to env vars

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 import history from "../history";
 
+
 export const getCurrUser = () => {
 	return async (dispatch) => {
-		const response = await axios.get("/api/current_user");
+		console.log("GET REQUEST:" + process.env.REACT_APP_BACKEND_URI + "/api/current_user");
+		const response = await axios.get(process.env.REACT_APP_BACKEND_URI + "/api/current_user");
 		console.log("action creator payload:")
 		console.log(response.data[0]);
 
